3cedb18a4166d31f02d2ab687045b5738dbebbdd
enginhu <erolengin@gmail.com>
2019-11-14 18:34:57 +0300

    Flask Blog Project With Blueprint

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -19,8 +19,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_POST'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "depodabekle@gmail.com"
-app.config['MAIL_PASSWORD'] = "O0o01384"
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config.from_object(__name__)
 mail = Mail(app)
 mail.init_app(app)
