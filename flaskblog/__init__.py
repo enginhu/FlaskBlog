24023f576ed7d656936fa1c67102acb0eeffba4b
enginhu <erolengin@gmail.com>
2019-11-14 17:37:35 +0300

    Initial commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '09fc4c35c731c7bb06d4092652a29cac'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db?check_same_thread=False'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['ADMINS'] = ['depodabekle@gmail.com', ]
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_POST'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "depodabekle@gmail.com"
+app.config['MAIL_PASSWORD'] = "O0o01384"
+app.config.from_object(__name__)
+mail = Mail(app)
+mail.init_app(app)
+
+
+from flaskblog.users.routes import users
+from flaskblog.main.routes import main
+from flaskblog.post.routes import posts
+
+
+app.register_blueprint(users)
+app.register_blueprint(main)
+app.register_blueprint(posts)
---
+++ flaskblog/users/utils.py
@@ -0,0 +1,37 @@
+import os
+import secrets
+
+from PIL import Image
+from flask import render_template, current_app
+from flask_mail import Message
+
+from flaskblog import app, mail
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    send_email('[Flask Blog] Reset Your Password',
+               sender=current_app.config['ADMINS'][0],
+               recipients=[user.email],
+               text_body=render_template('email/reset_password.txt', user=user, token=token),
+               html_body=render_template('email/reset_password.html', user=user, token=token))
